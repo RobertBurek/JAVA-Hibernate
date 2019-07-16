36951f91d870a6adb4d1104fbbcf2e0bdf272391
Robert Burek <robertburek@wp.pl>
2019-07-16 22:53:25 +0200

    @OneToMany jednokierunkowe - przygotowanie aplikacji i encji

---
+++ HibernateAssociationsDemo3/src/main/java/entity/Department.java
@@ -0,0 +1,31 @@
+package entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * Created by Robert Burek
+ */
+@Entity
+@Table(name = "department")
+@NoArgsConstructor
+@ToString
+public class Department {
+
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_department")
+    private Integer idDepartment;
+
+    @Getter
+    @Setter
+    @Column(name = "name")
+    private String name;
+
+}
